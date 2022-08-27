c94e61a555125f31fa169dc9149332e4771cecf5
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-27 10:01:51 +0200

    + Updated copyright info in license + Update license disclaimer

--- src/Osynapsy/Html/Helper/JQuery.php
+++ src/Osynapsy/Html/Helper/JQuery.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Osynapsy package.
  *
- * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,26 +14,26 @@ namespace Osynapsy\Html\Helper;
 /**
  * Description of JQuery
  *
- * @author Pietro Celeste
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class JQuery
 {
     private $elements = array();
     private $selector = '';
     private $response;
-    
+
     public function __construct($selector, $response = null)
     {
         $this->selector = $selector;
         $this->response = $response;
     }
-    
+
     public function __call($method, $params)
     {
         $this->elements[$method] = $params;
         return $this;
     }
-    
+
     public function __toString()
     {
         $string = '$(\''.$this->selector.'\')';
@@ -42,16 +42,16 @@ class JQuery
         }
         return $string;
     }
-    
+
     private function buildMethod($method, $parameters)
     {
         if (empty($parameters)) {
-            return ".{$method}())";        
+            return ".{$method}())";
         }
         $pars = implode(
-            ",", 
+            ",",
             array_map(
-                function($value) { 
+                function($value) {
                     switch (gettype($value)) {
                         case 'boolean':
                             return empty($value) ? 'false' : 'true';
@@ -59,15 +59,15 @@ class JQuery
                         case 'double' :
                             return $value;
                         default:
-                            return "'".addslashes($value)."'";                    
+                            return "'".addslashes($value)."'";
                     }
-                }, 
+                },
                 $parameters
             )
         );
         return '.'.$method."($pars)";
     }
-    
+
     public function exec()
     {
         $this->response->js($this->__toString());
